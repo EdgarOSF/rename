269862e214479935e66215ed427942287ca315f7
EdgarOSF <edgaromarsf@gmail.com>
2026-06-24 17:48:57 -0600

    deteccionde video y subtitulo

--- main.py
+++ main.py
@@ -1,23 +1,50 @@
 import os
 import sys
+import re
 from pathlib import Path
 
 def main():
     # print( Path.cwd() )
     path_files = sys.argv[1]
-    # names_files = os.listdir(path_files)
-    
-    p = Path(path_files)
-    for file in p.glob('*mkv'):
-        print(file)
-
-
     # print(os.listdir(path_files))
-    
+
+    # names_files = os.listdir(path_files)
+    suffixes_videos = ['.mkv', '.mp4']
+    suffix_subtitle = '.srt'
+    video_info = {}
+    name_file = []
+    p = Path(path_files)
+    temp = {}
+
+    for file in p.glob('*'):
+        if file.suffix in suffixes_videos:
+            temp['video'] = file.stem
+        if file.suffix == suffix_subtitle:
+            temp['subtitle'] = file.stem
+        if temp['video'] and temp['subtitle']:
+            pass
 
 
-    # print(sys.argv[1])
- 
+    video_info['cap'] = temp
+
+    print(video_info)
+
+    # Determinar numero de capitulo
+    # Determinar subtitulo del numero de capitulo
+
+
+def obtener_temporada(name_file):
+    temp_regex = re.compile(r'(S|s)\d\d')
+    mo = temp_regex.search(name_file)
+    print('La temporada es:', mo.group())
+
+def obtener_capitulo(name_file):
+    capitulo_regex = re.compile(r'(E|e)\d\d')
+    mo = capitulo_regex.search(name_file)
+    print('El capitulo es:', mo.group())
+
+
+
 
 if __name__ == "__main__":
     main()
